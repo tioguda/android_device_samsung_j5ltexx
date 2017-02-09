e9903bb687ab578f0b76fc39046312b99d3feba6
SoUnd001 <sound0020@gmail.com>
2017-02-09 19:23:07 +0200

    Fix derp

--- lineage.mk
+++ lineage.mk
@@ -5,5 +5,4 @@ $(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5ltexx TARGET_DEVICE=j5lte
 
-<<<<<<< HEAD:cm.mk
 PRODUCT_NAME := lineage_j5ltexx
