f5cc865accef936e8ba89dce56be48cbb648bf96
tioguda <guda.flavio@gmail.com>
2017-05-29 14:55:39 -0300

    Tesla Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_j5ltexx.mk \
+    $(LOCAL_DIR)/tesla.mk 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo lineage_j5ltexx-userdebug
-add_lunch_combo lineage_j5ltexx-eng
+add_lunch_combo tesla_j5ltexx-userdebug
+add_lunch_combo tesla_j5ltexx-eng
