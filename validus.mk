d0da6c3600dec7be143881f4f4ce0f2e0887ab51
tioguda <guda.flavio@gmail.com>
2017-06-13 20:07:47 -0300

    Add Device Maintainers

--- validus.mk
+++ validus.mk
@@ -7,3 +7,7 @@ $(call inherit-product, device/samsung/j5ltexx/full_j5ltexx.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5ltexx TARGET_DEVICE=j5lte
 
 PRODUCT_NAME := validus_j5ltexx
+
+# Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="tioguda"
